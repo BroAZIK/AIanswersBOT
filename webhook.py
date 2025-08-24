8f4fbe9541f5754c02a8911e3b7c0046c81af1e5
BroAZIK-DJANGO <azizbektolliboyev155@gmail.com>
2025-08-24 15:16:22 +0500

    done

--- webhook.py
+++ webhook.py
@@ -1,30 +1,3 @@
-from telegram import Bot
-from settings import *
-
-bot = Bot(token=TOKEN)
 
 
-def get_info():
-    """Webhook haqida ma'lumot olish"""
-    info = bot.get_webhook_info()
-    print("Webhook info:", info)
 
-
-def delete():
-    """Webhookni o‘chirish"""
-    result = bot.delete_webhook()
-    print("Webhook o‘chirildi:", result)
-
-
-async def set():
-    """Webhookni sozlash"""
-    url = f"https://iqmatebot.pythonanywhere.com/webhook/{TOKEN}"
-    result = await bot.set_webhook(url=url)
-    print("Webhook o‘rnatildi:", result)
-
-
-import asyncio
-
-if __name__ == "__main__":
-    # delete()  # agar sync bo'lsa
-    asyncio.run(set())
